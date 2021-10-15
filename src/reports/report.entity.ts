8cae66e0d50c09ebc35f6f03f077c0375aa141cc
mavi <mavi.baris96@gmail.com>
2021-10-15 04:22:54 +0300

    create dtos and define ValidationPipe

---
+++ src/reports/report.entity.ts
@@ -0,0 +1,9 @@
+import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class Report {
+  @PrimaryGeneratedColumn()
+  id: number;
+  @Column()
+  price: number;
+}
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,7 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post } from '@nestjs/common';
 
-@Controller('users')
-export class UsersController {}
+@Controller('auth')
+export class UsersController {
+  @Post('/signup')
+  createUser() {}
+}
