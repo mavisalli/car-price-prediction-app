d8dd0b53057f5cf6dab88b6bae360af8a05f4d7c
mavi <mavi.baris96@gmail.com>
2021-10-16 21:31:46 +0300

    add logical operations related to interceptor

---
+++ src/interceptors/serialize.interceptor.ts
@@ -0,0 +1,32 @@
+import {
+  UseInterceptors,
+  NestInterceptor,
+  ExecutionContext,
+  CallHandler,
+  Next,
+} from '@nestjs/common';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { plainToClass } from 'class-transformer';
+
+interface ClassConstructor {
+  //security
+  new (...args: any[]): {};
+}
+
+export function Serialize(dto: ClassConstructor) {
+  return UseInterceptors(new SerializeInterceptor(dto));
+}
+
+export class SerializeInterceptor implements NestInterceptor {
+  constructor(private dto: any) {}
+  intercept(context: ExecutionContext, handle: CallHandler): Observable<any> {
+    return handle.handle().pipe(
+      map((data: any) => {
+        return plainToClass(this.dto, data, {
+          excludeExtraneousValues: true,
+        });
+      }),
+    );
+  }
+}
