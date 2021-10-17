59fdc822d2ccb58536d4dd341a9dd3a84e5ed300
mavi <mavi.baris96@gmail.com>
2021-10-17 16:39:32 +0300

    Revert "update"
    This reverts commit badcbd16facb57b299df3dd2e06e3a384d4ca582.

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,6 +1,5 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { AuthService } from './auth.service';
 import { User } from './user.entity';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
@@ -8,6 +7,6 @@ import { UsersService } from './users.service';
 @Module({
   imports: [TypeOrmModule.forFeature([User])],
   controllers: [UsersController],
-  providers: [UsersService, AuthService],
+  providers: [UsersService],
 })
 export class UsersModule {}
