968235c94dc9e7fca29aafa04f0172f7ba411985
mavi <mavi.baris96@gmail.com>
2021-10-20 03:28:58 +0300

    apply testing for findUser and findAllUsers

--- src/users/users.controller.spec.ts
+++ src/users/users.controller.spec.ts
@@ -1,12 +1,47 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { AuthService } from './auth.service';
+import { User } from './user.entity';
 import { UsersController } from './users.controller';
+import { UsersService } from './users.service';
 
 describe('UsersController', () => {
   let controller: UsersController;
+  let fakeUsersService: Partial<UsersService>;
+  let fakeAuthService: Partial<AuthService>;
 
   beforeEach(async () => {
+    fakeUsersService = {
+      findOne: (id: number) => {
+        return Promise.resolve({
+          id,
+          email: 'asdf@asdf.com',
+          password: '12345',
+        } as User); //arguman olarak ne veriyorsak onu altta aynı sekilde döndürmeliyiz
+      },
+      find: (email: string) => {
+        return Promise.resolve([
+          { id: 1, email, password: 'adasdgdg' } as User,
+        ]);
+      },
+      // remove: ()=> {},
+      // update: ()=> {},
+    };
+    fakeAuthService = {
+      // signup: () => {},
+      // signin: () => {},
+    };
     const module: TestingModule = await Test.createTestingModule({
       controllers: [UsersController],
+      providers: [
+        {
+          provide: UsersService,
+          useValue: fakeUsersService,
+        },
+        {
+          provide: AuthService,
+          useValue: fakeAuthService,
+        },
+      ],
     }).compile();
 
     controller = module.get<UsersController>(UsersController);
