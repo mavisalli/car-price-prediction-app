b1cd2561e050062f3743875c580d0fc58f5dc090
mavi <mavi.baris96@gmail.com>
2021-10-25 19:59:45 +0300

    Create reports entity

--- src/reports/report.entity.ts
+++ src/reports/report.entity.ts
@@ -4,6 +4,25 @@ import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
 export class Report {
   @PrimaryGeneratedColumn()
   id: number;
+
   @Column()
   price: number;
+
+  @Column()
+  make: string;
+
+  @Column()
+  model: string;
+
+  @Column()
+  year: number;
+
+  @Column()
+  lng: number;
+
+  @Column()
+  lat: number;
+
+  @Column()
+  mileage: number;
 }
