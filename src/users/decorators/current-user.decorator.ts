57f985bfe262ec574de75fc5c7312f204572e82c
mavi <mavi.baris96@gmail.com>
2021-10-27 01:05:58 +0300

    Update

--- src/users/decorators/current-user.decorator.ts
+++ src/users/decorators/current-user.decorator.ts
@@ -3,6 +3,6 @@ import { createParamDecorator, ExecutionContext } from '@nestjs/common';
 export const CurrentUser = createParamDecorator(
   (data: never, context: ExecutionContext) => {
     const request = context.switchToHttp().getRequest();
-    return request.CurrentUser;
+    return request.currentUser;
   },
 );
