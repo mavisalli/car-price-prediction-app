f58ce4db3d9ada9073f1894032b8259598fdd265
mavi <mavi.baris96@gmail.com>
2021-10-27 21:41:08 +0300

    Create migration structure

---
+++ ormconfig.js
@@ -0,0 +1,40 @@
+var dbConfig = {
+  synchronize: false,
+  migrations: ['migrations/*.js'],
+  cli: {
+    migrationsDir: 'migrations',
+  },
+};
+
+switch (process.env.NODE_ENV) {
+  case 'development':
+    Object.assign(dbConfig, {
+      type: 'sqlite',
+      database: 'db.sqlite',
+      entities: ['**/*.entity.js'],
+    });
+    break;
+  case 'test':
+    Object.assign(dbConfig, {
+      type: 'sqlite',
+      database: 'test.sqlite',
+      entities: ['**/*.entity.ts'],
+      migrationsRun: true, // her bir test icin tekrardan veritabanı olustur
+    });
+    break;
+  case 'production':
+    Object.assign(dbConfig, {
+      type: 'postgres',
+      url: process.env.DATABASE_URL,
+      migrationsRun: true,
+      entities: ['**/*.entity.js'],
+      ssl: {
+        rejectUnauthorized: false,
+      },
+    });
+    break;
+  default:
+    throw new Error('unknown environment');
+}
+
+module.exports = dbConfig;
